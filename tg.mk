73e806e632918fb6ee081a925f62f4183f4c0950
dhacker29 <davidhackerdvm@gmail.com>
2013-11-27 03:35:36 -0600

    Gummify
    Change-Id: I2e5ea4d2cfaa663c1a5107b0b5ddba6bfae7f78f

--- tg.mk
+++ tg.mk
@@ -5,18 +5,18 @@ PRODUCT_RELEASE_NAME := GN-CDMA
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Gummy stuff.
+$(call inherit-product, vendor/Gummy/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/toroplus/aosp_toroplus.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/Gummy/config/nfc_enhanced.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := toroplus
-PRODUCT_NAME := cm_toroplus
+PRODUCT_NAME := tg_toroplus
 PRODUCT_BRAND := Google
 PRODUCT_MODEL := Galaxy Nexus
 PRODUCT_MANUFACTURER := Samsung
